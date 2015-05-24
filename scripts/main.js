dcea7199cdcae2aa4f8bd5717f567f9a6d08427c
Teddy <Teddy@mail.com>
2015-05-24 10:20:07 +1000

    backup

--- scripts/main.js
+++ scripts/main.js
@@ -2,19 +2,30 @@
 
 var app = angular.module('snapApp', []);
 
-app.controller('MainCtrl', function ($scope,$timeout) {
+app.controller('MainCtrl', function ($scope, $timeout) {
 
     //get players cards
     var playerCards = [];
     var cpuCards = [];
-    var cards_centre_pile=[];
+    var cards_centre_pile = [];
 
-    $scope.reactionTime=2000;
+    $scope.reactionTime = 1000;
 
-    $scope.startGame = function (playerToStart ) {
-        var playerToStart = Math.random() >0.5 ? 1 :0;
-        $scope.snap=false;
-        cards_centre_pile=[];
+    //set randomly which player starts
+    $scope.playerTurn = Math.random() > 0.5 ? 1 : 0;
+
+    //divide cards for players
+    //[] playerCards: card for player
+    //[] cpuCards: cards for cpu
+    //int playerTurn:randomly seelct starting player
+    $scope.startGame = function () {
+        //debugger;
+
+        console.log('cpuCards.length', cpuCards.length);
+        console.log('player.length', playerCards.length);
+
+        $scope.snap = false;
+        cards_centre_pile = [];
         console.log('start game');
 
         var nrs = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
@@ -22,7 +33,7 @@ app.controller('MainCtrl', function ($scope,$timeout) {
 
         nrs.forEach(function (nr) {
             suits.forEach(function (st) {
-                if (st=='c' || st=='h')
+                if (st == 'c' || st == 'h')
                     playerCards.push({number: nr, suit: st})
                 else
                     cpuCards.push({number: nr, suit: st})
@@ -30,16 +41,21 @@ app.controller('MainCtrl', function ($scope,$timeout) {
             });
         });
 
-        $scope.pickCard(playerToStart);
-
+        //start to pick card when cpu
+        if ($scope.playerTurn==1) {
+            $scope.pickCard(1);
+        }
     }
 
+    //int p: player=0 , cpu=1
+    //lastPickedCard {number:, suit:}
+    //lastPickedCardOld {}:predessor of lastPickedCard
     $scope.pickCard = function (p) {
-        console.log('picks card:', p);
-        var lastPickedCardOld = ($scope.lastPickedCard!=null) ?  $scope.lastPickedCard : 0;
+        console.log('picks card:', $scope.playerTurn);
+        var lastPickedCardOld = ($scope.lastPickedCard != null) ? $scope.lastPickedCard : 0;
 
         //debugger;
-        if (p == 'player') {
+        if ($scope.playerTurn == 0) {
             $scope.lastPickedCard = playerCards[0];
 
             //remove first card from player stack
@@ -48,7 +64,7 @@ app.controller('MainCtrl', function ($scope,$timeout) {
             cards_centre_pile.push(playerCards[0]);
         }
 
-        if (p == 'cpu') {
+        if ($scope.playerTurn == 1) {
             $scope.lastPickedCard = cpuCards[0];
 
             //remove first card from cpu stack
@@ -57,29 +73,47 @@ app.controller('MainCtrl', function ($scope,$timeout) {
             cards_centre_pile.push(cpuCards[0]);
         }
 
+
+
         if (lastPickedCardOld.number == $scope.lastPickedCard.number) {
             //cpu calls snap with reacction time
-            $timeout(function()
-            {
-                $scope.callSnap(1)
-            }
-                ,$scope.reactionTime);
+            $timeout(function () {
+                    $scope.callSnap(1)
+                }
+                , $scope.reactionTime);
             //console.log('cpu');
         }
+
+        else
+        {
+            $scope.playerTurn=!$scope.playerTurn;
+        }
+
+
+
+
     }
 
-    $scope.callSnap=function(p){
+    $scope.callSnap = function (p) {
         if (!$scope.snap) {
             $scope.snap = true;
             console.log('the winner is:', p);
 
             //winner takes the cards
-            p==0 ?  playerCards.concat(cards_centre_pile) : cpuCards.concat(cards_centre_pile);
+            p == 0 ? playerCards.concat(cards_centre_pile) : cpuCards.concat(cards_centre_pile);
 
-            console.log(cpuCards.length);
+            console.log('cpuCards.length', cpuCards.length);
+            console.log('player.length', playerCards.length);
+
+            if (playerCards.length == 0) {
+                console.log('cpu is the winner')
+            }
+
+            if (cpuCards.length == 0) {
+                console.log('player is the winner');
+            }
         }
     }
-
 
 
 })
